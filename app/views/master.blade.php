14dbd54d7e34560b4fb729a8341a34bedeadc65f
Marcin <marcin19962@onet.eu>
2014-03-04 20:07:57 +0100

    Added bootstrap, master template and icons.

---
+++ app/views/master.blade.php
@@ -0,0 +1,22 @@
+<head>
+
+    <link rel="stylesheet" href="/css/bootstrap.css" />
+    <link rel="stylesheet" href="/css/bootstrap-theme.css" />
+    <script src="/js/jquery-2.0.3.min.js"></script>
+    <script src="/js/bootstrap.min.js"></script>
+    <script src="/js/main.js"></script>
+    <script src="/js/bootstrap.datepicker.js"></script>
+    <link rel="stylesheet" href="/css/group.css" />
+    <link rel="stylesheet" href="/css/main.css" />
+    <script>
+        (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+            (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+            m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+        })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+        ga('create', 'UA-21965013-5', 'hiroto.eu');
+        ga('send', 'pageview');
+
+    </script>
+</head>
+@yield ('content')
