814f52f485bcb97baababcc61607c6d2677192aa
Marcin <marcin19962@onet.eu>
2014-04-07 22:28:44 +0200

    Add nice welcoming message to index.

--- app/views/index.blade.php
+++ app/views/index.blade.php
@@ -1,6 +1,6 @@
 @extends('master')
 @section('content')
 <div class="container">
-    <h1>Welcome!</h1>
+    <h1>Welcome {{Auth::check() ? Auth::user()->name."!" : "stranger! Please log in to create new skins or vote up existing one."}} </h1>
 </div>
 @stop
