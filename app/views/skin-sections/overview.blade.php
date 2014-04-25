478596435c21f0fe4237b0a53ec2a8031efab403
Marcin <marcin19962@onet.eu>
2014-04-25 15:48:45 +0200

    Remove header from description
    Add htmlpurifier and markdown parser to dependencies.

--- app/views/skin-sections/overview.blade.php
+++ app/views/skin-sections/overview.blade.php
@@ -13,7 +13,7 @@
     </div>
 </div>
 <div class="panel panel-primary">
-    <div class="panel-heading">Creator's words</div>
+    <!--<div class="panel-heading">Creator's words</div>-->
     <div class="panel-body">
         {{$skin->description}}
     </div>
