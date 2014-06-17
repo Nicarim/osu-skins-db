be9cf4b7f880bf89c12f22c5394016ffe7f4472d
Marcin <marcin19962@onet.eu>
2014-06-17 16:36:19 +0200

    Fix wrong value being applied

--- public/js/main.js
+++ public/js/main.js
@@ -84,9 +84,9 @@ $(document).ready(function() {
             startext.text(typeOfStar ? "Unstar" : "Star");
             startext.animate({opacity: 1}, 1000);
             if(typeOfStar)
-                $("#votes-count").text(parseInt($("#votes-count").text()) - 1);
-            else
                 $("#votes-count").text(parseInt($("#votes-count").text()) + 1);
+            else
+                $("#votes-count").text(parseInt($("#votes-count").text()) - 1);
         });
     });
 });
