fcfb7b8a569a6338eb37428708f7607cf8e096b1
Marcin <marcin19962@onet.eu>
2014-07-03 18:57:05 +0200

    Fix jquery not being able to select nested manager.

--- public/js/main.js
+++ public/js/main.js
@@ -171,7 +171,7 @@ function populateFilemanager(target){
     }
 }
 function populateAnimatable(target){
-    var el = $(target).find(".nested-manager:first");
+    var el = $(target).parent().find(".nested-manager:first");
     var wasEmpty = isEmpty(el);
     var contentLink = '/skins/view/' + $("#filesmanager-link").data('skinid') + "/animations?f=" + $(target).data('filename') + "&hd=" + $(target).data('ishd');
     if (wasEmpty){
