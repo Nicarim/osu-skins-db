fb4dd79d3493c9ce094035f2f191df036f8ad5fb
marcin19962 <marcin19962@onet.eu>
2014-07-10 19:05:55 +0200

    Fix mobile responsiveness. Resolves #18

--- app/views/navbar.blade.php
+++ app/views/navbar.blade.php
@@ -1,9 +1,15 @@
 <nav class="navbar navbar-default navbar-inverse navbar-fixed-top" role="navigation">
     <div class="container">
     <div class="navbar-header">
+        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navbar-toggle">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+        </button>
         <a class="navbar-brand" href="/">Skins Database</a>
     </div>
-        <div class="collapse navbar-collapse">
+        <div class="collapse navbar-collapse" id="navbar-toggle">
             <ul class="nav navbar-nav">
                 <li class="dropdown">
                     <a href="#" class="dropdown-toggle" data-toggle="dropdown">Skins <b class="caret"></b></a>
