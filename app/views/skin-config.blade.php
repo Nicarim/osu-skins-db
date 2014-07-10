783c8dbfb38fec3c2796c939ad0f2400ee939ed4
marcin19962 <marcin19962@onet.eu>
2014-07-10 19:08:11 +0200

    Remove nsfw toggle.

--- app/views/skin-config.blade.php
+++ app/views/skin-config.blade.php
@@ -11,13 +11,6 @@
             </label>
             <textarea name="description" id="overviewedit" class="form-control"
                       rows="20">{{isset($skin) ? $skin->description : ''}}</textarea>
-            <div class="checkbox">
-                <label for="warnnsfw">
-                    Mark as NSFW content
-                </label>
-                <input type="checkbox" id="warnnsfw" name="warnnsfw" value="1"/>
-                <small><br/>Check this, if your skin is not 13+ save.</small>
-            </div>
             <div class="form-group">
                 <label for="thumbnail">Thumbnail of your skin</label>
                 <input class="form-control" type="file" id="thumbnail" name="thumbnail">
